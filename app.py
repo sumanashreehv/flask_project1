c6e7ef36ce8ce30cbb9c16514a8066c985c220de
Sumanashree <sumanashreekrao@gmail.com>
2024-09-30 18:43:13 +0530

    Html file added

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "My first flask project";
+    return render_template("home.html"); 
 
 
 if __name__ == "__main__":
