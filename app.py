2a5e791b8b0d78b52bcd2d3a53ee9ce1c3518b29
Sumanashree <sumanashreekrao@gmail.com>
2024-10-11 14:54:51 +0530

    Flask app improvement

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect,url_for
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("home.html"); 
+def home():
+    return "Hello!! New to Flask <h1>WEB DEVELOPMENT</h1>";
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!";
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"));
+
+@app.route("/jumbled")
+def hello_world():
+    return render_template("jumbledword.html"); 
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True)
